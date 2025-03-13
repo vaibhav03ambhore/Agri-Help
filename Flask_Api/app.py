63f78e7f2ee0f663a06a0dd9efd4ba3895cb29d8
Vaibhav Ambhore <115728010+vaibhav03ambhore@users.noreply.github.com>
2025-03-13 12:21:38 +0530

    Update app.py

--- Flask_Api/app.py
+++ Flask_Api/app.py
@@ -20,4 +20,5 @@ def home():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 4000))
+    print(f"Starting app on port {port}")
     app.run(host='0.0.0.0', port=port, debug=True)
