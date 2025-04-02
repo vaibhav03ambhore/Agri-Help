af8ee0f88b5325219e1cdcf09ece36cffb43150b
vaibhav03ambhore <vaibhavambhore803@gmail.com>
2025-04-02 12:18:21 +0530

    loggedIn use can go directly to dashboard from home page

--- AgriHelp_web_app/Frontend/vite.config.js
+++ AgriHelp_web_app/Frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        // target: 'https://agri-help-backend.onrender.com',
-        target: 'http://localhost:5000',
+        target: 'https://agri-help-backend.onrender.com',
+        // target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       },
