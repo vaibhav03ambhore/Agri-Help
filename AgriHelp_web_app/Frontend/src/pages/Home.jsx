e8f55abb0b5506888b66ba10db4af577c24fe98d
vaibhav03ambhore <vaibhavambhore803@gmail.com>
2025-04-02 18:08:01 +0530

    (loading homepage) issue-fix-1

--- AgriHelp_web_app/Frontend/src/pages/Home.jsx
+++ AgriHelp_web_app/Frontend/src/pages/Home.jsx
@@ -196,7 +196,7 @@ const Home = () => {
       <div className="min-h-screen flex items-center justify-center">
         <div className="text-center">
           <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-[#4a8b3f] mx-auto mb-4"></div>
-          <p className="text-gray-600">Loading your dashboard...</p>
+          <p className="text-gray-600">Loading your Homepage...</p>
         </div>
       </div>
     );
