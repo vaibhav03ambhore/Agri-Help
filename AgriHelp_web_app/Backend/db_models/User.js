af4d1dfd22d30b24d131f1c5f3ebb5d7802ae115
vaibhav03ambhore <vaibhavambhore803@gmail.com>
2025-04-02 17:39:10 +0530

    email or mobile (require field missing) issue

--- AgriHelp_web_app/Backend/db_models/User.js
+++ AgriHelp_web_app/Backend/db_models/User.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     basicInfo: {
       fullName: { type: String, required: true },
-      email: { type: String, required: true, unique: true },
-      contactNumber: { type: String, required: true },
+      email: { type: String, unique: true },
+      contactNumber: { type: String  },
       location: { type: String, required: true }
     },
     farmDetails: {
